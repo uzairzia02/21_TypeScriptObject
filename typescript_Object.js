6120323bae81a9a197879fc6ef95c04c5ee3345f
uzairzia <uzair.governorsindh@gmail.com>
2024-03-01 17:36:39 +0500

    first commit

---
+++ typescript_Object.js
@@ -0,0 +1,9 @@
+"use strict";
+// Create an object 'person' that conforms to the 'person' interface
+let person = {
+    age: 28,
+    name: 'Uzair Zia',
+    department: 'HR',
+    gender: true,
+};
+console.log(person);
---
+++ typescript_Object.ts
@@ -0,0 +1,21 @@
+
+
+
+// Create an object 'person' that conforms to the 'person' interface
+let person:person = {
+    age : 28,
+    name : 'Uzair Zia',
+    department: 'HR',
+    gender: true,
+}
+
+// Define an interface named 'person'
+interface person{
+    age: number,
+    name: string,
+    department: string,
+    gender: boolean,
+
+}
+
+console.log(person);
